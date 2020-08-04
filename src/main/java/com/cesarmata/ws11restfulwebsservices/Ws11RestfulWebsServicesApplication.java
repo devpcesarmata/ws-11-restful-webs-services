96b7f000b173381c44de3a71ab53bf8af116a5a5
César <dev.pcesarmata@gmail.com>
2020-08-04 09:34:07 -0500

    init commit

---
+++ src/main/java/com/cesarmata/ws11restfulwebsservices/Ws11RestfulWebsServicesApplication.java
@@ -0,0 +1,13 @@
+package com.cesarmata.ws11restfulwebsservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ws11RestfulWebsServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ws11RestfulWebsServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cesarmata/ws11restfulwebsservices/Ws11RestfulWebsServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesarmata.ws11restfulwebsservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ws11RestfulWebsServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
